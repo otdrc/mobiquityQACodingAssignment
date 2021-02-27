d82c940500017b6f1185eba3e88cf5166bd65bad
DESKTOP-36DNEUQ\toder <todericaolesea@gmail.com>
2021-02-27 15:55:47 +0200

    Used models (could have used something like Lombok to simplify the classes, but it is fine with IntelliJ generated code too)

--- src/main/java/objectModels/Company.java
+++ src/main/java/objectModels/Company.java
@@ -1,48 +1,8 @@
 package objectModels;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Company {
-    private String name;
-    private String catchPhrase;
-    private String bs;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCatchPhrase() {
-        return catchPhrase;
-    }
-
-    public void setCatchPhrase(String catchPhrase) {
-        this.catchPhrase = catchPhrase;
-    }
-
-    public String getBs() {
-        return bs;
-    }
-
-    public void setBs(String bs) {
-        this.bs = bs;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, catchPhrase, bs);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Company)) return false;
-        Company company = (Company) o;
-        return name.equals(company.name) &&
-                catchPhrase.equals(company.catchPhrase) &&
-                bs.equals(company.bs);
-    }
+    private String name, catchPhrase, bs;
 }
--- src/main/java/objectModels/Geo.java
+++ src/main/java/objectModels/Geo.java
@@ -1,38 +1,8 @@
 package objectModels;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Geo {
-    private String lat;
-    private String lng;
-
-    public String getLat() {
-        return lat;
-    }
-
-    public void setLat(String lat) {
-        this.lat = lat;
-    }
-
-    public String getLng() {
-        return lng;
-    }
-
-    public void setLng(String lng) {
-        this.lng = lng;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(lat, lng);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Geo)) return false;
-        Geo geo = (Geo) o;
-        return lat.equals(geo.lat) &&
-                lng.equals(geo.lng);
-    }
+    private String lat, lng;
 }
